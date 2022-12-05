a07e55cb3f03e345f50e9de13cb1139ba15aa1f8
Marcin Zawiejski <dragmz@gmail.com>
2022-12-05 22:38:42 +0100

    install/update tools command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,8 @@
 ## v0.0.1
-Initial release
+- Initial release
+
+## v0.0.2
+- Extension activation fixes
+
+## v0.0.3
+- Experimental "TEAL: Install/Update Tools" command
--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,37 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import cp = require('child_process');
 import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
 } from 'vscode-languageclient/node';
 
+let out = vscode.window.createOutputChannel("TEAL");
+
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand("teal.tools.install", () => {
+		out.clear();
+		out.show();
+		setTimeout(() => {
+			const tools = ["github.com/dragmz/teal/cmd/tealsp@latest"];
+			tools.forEach(tool => {
+				out.appendLine("Installing: " + tool);
+				cp.execFileSync("go", ["install", "github.com/dragmz/teal/cmd/tealsp@latest"]);
+			});
+			out.appendLine("Done.");
+		}, 100);
+	}));
+
 	let serverOptions: ServerOptions = {
-		command: "tealsp"
+		run: {
+			command: "tealsp"
+		},
+		debug: {
+			command: "tealsp",
+			args: ["-debug", "tealsp_debug.log"]
+		}
 	};
 
 	// Options to control the language client
