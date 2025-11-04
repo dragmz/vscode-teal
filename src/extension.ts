c2ee69dabf317e5a73f7dbed90772baf16f6c132
Marcin Zawiejski <dragmz@gmail.com>
2025-11-04 23:07:20 +0100

    callbar

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,8 @@ import {
 } from 'vscode-languageclient/node';
 
 import fs = require('fs');
+import algosdk, { SignedTransaction } from 'algosdk';
+import { modelsv2 } from 'algosdk';
 
 const ext = process.platform === "win32" ? ".exe" : "";
 
@@ -132,8 +134,216 @@ async function tryUpdate(context: vscode.ExtensionContext, client: LanguageClien
 	}
 }
 
+function getNetworkUrl(network: string): string | undefined {
+	switch (network) {
+		case "Mainnet":
+			return "https://mainnet-api.4160.nodely.dev";
+		case "Testnet":
+			return "https://testnet-api.4160.nodely.dev";
+		case "Betanet":
+			return "https://betanet-api.4160.nodely.dev";
+		default:
+			return undefined;
+	}
+}
+
+function pickNetworkAndReturnUrl(): Thenable<string | undefined> {
+	return vscode.window.showQuickPick(["Mainnet", "Testnet", "Betanet", "Custom"], {
+		placeHolder: "Select the network"
+	}).then(async (network) => {
+		if (network === undefined) {
+			return undefined;
+		}
+		if (network === "Custom") {
+			return await vscode.window.showInputBox({
+				prompt: "Enter the custom network node URL",
+				placeHolder: "e.g. https://custom-algorand-node.com"
+			});
+		} else {
+			return getNetworkUrl(network);
+		}
+	});
+}
+
+export class CallBarProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'callbarview';
+	constructor(private readonly _context: vscode.ExtensionContext) { }
+
+	public async resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		webviewView.webview.options = {
+			enableScripts: true,
+		};
+		webviewView.webview.html = await this.getHtmlForWebview(webviewView.webview);
+		webviewView.webview.onDidReceiveMessage(async message => {
+			switch (message.command) {
+				case 'debug':
+					{
+						const network = message.network;
+						const id = message.id;
+
+						const url = getNetworkUrl(network);
+						if (url === undefined) {
+							vscode.window.showErrorMessage(`Invalid network selected: ${network}`);
+							return;
+						}
+
+						// get LanguageClient from extension context
+						const client = this._context.subscriptions.find(sub => sub instanceof LanguageClient) as LanguageClient;
+						if (!client) {
+							vscode.window.showErrorMessage("Language client not found.");
+							return;
+						}
+
+						const content = await decompile(client, url, id);
+						if (!content) {
+							vscode.window.showErrorMessage(`Failed to decompile program ID ${id}.`);
+							return;
+						}
+
+						const filename = `App_${id}.teal`;
+						// get workspace folder
+						const workspaceFolders = vscode.workspace.workspaceFolders;
+						if (!workspaceFolders || workspaceFolders.length === 0) {
+							vscode.window.showErrorMessage("No workspace folder found to save the decompiled file.");
+							return;
+						}
+						const folder = workspaceFolders[0];
+						const fileUri = vscode.Uri.joinPath(folder.uri, filename);
+						fs.writeFileSync(fileUri.fsPath, content);
+
+						// open the file in vscode
+						const doc = await vscode.workspace.openTextDocument(fileUri);
+						await vscode.window.showTextDocument(doc);
+
+						const sourcemap = await generateSourcemap(client);
+						if (!sourcemap) {
+							return;
+						}
+
+						const sourcemapFilename = `App_${id}.tok.map`;
+						const sourcemapFileUri = vscode.Uri.joinPath(folder.uri, sourcemapFilename);
+						fs.writeFileSync(sourcemapFileUri.fsPath, sourcemap);
+
+						const trace = await call(url, id);
+						if (!trace) {
+							return;
+						}
+
+						const traceFilename = `App_${id}.avm.trace.json`;
+						const traceFileUri = vscode.Uri.joinPath(folder.uri, traceFilename);
+						fs.writeFileSync(traceFileUri.fsPath, trace);
+
+						const traceDoc = await vscode.workspace.openTextDocument(traceFileUri);
+						await vscode.window.showTextDocument(traceDoc);
+
+						await vscode.commands.executeCommand("extension.avmDebugger.debugOpenTraceFile");
+					}
+			}
+		});
+	}
+
+	private async getHtmlForWebview(webview: vscode.Webview): Promise<string> {
+		const htmlUri = vscode.Uri.joinPath(this._context.extensionUri, 'media', 'callbar.html');
+		const htmlBuffer = await vscode.workspace.fs.readFile(htmlUri);
+		return htmlBuffer.toString();
+	}
+}
+
+async function generateSourcemap(client: LanguageClient): Promise<any | null> {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showErrorMessage("No active editor found.");
+		return;
+	}
+
+	const response = await client.sendRequest("workspace/executeCommand", {
+		command: "teal.sourcemap.generate",
+		arguments: {
+			uri: editor.document.uri.toString(),
+		}
+	}) as { sourcemap: any } | null;
+
+	if (!response) {
+		vscode.window.showErrorMessage("Failed to generate source map for the current document.");
+		return;
+	}
+
+	response.sourcemap.sources = [editor.document.uri.fsPath];
+
+	const content = JSON.stringify(response.sourcemap, null, 2);
+	return content;
+}
+
+async function call(url: string, id: string): Promise<string | null> {
+	const ac = new algosdk.Algodv2("", url);
+
+	const appIndex = BigInt(id);
+	const suggestedParams = await ac.getTransactionParams().do();
+	const sender = "Y76M3MSY6DKBRHBL7C3NNDXGS5IIMQVQVUAB6MP4XEMMGVF2QWNPL226CA";
+
+	const call_tx = algosdk.makeApplicationNoOpTxnFromObject({
+		appIndex,
+		sender,
+		suggestedParams,
+	});
+
+	const txns = [call_tx];
+	const group = algosdk.assignGroupID(txns);
+
+	const request = new modelsv2.SimulateRequest({
+		txnGroups: [
+			new modelsv2.SimulateRequestTransactionGroup({
+				txns: group.map(txn => new SignedTransaction({ txn })),
+			})
+		],
+		allowEmptySignatures: true,
+		allowMoreLogging: true,
+		allowUnnamedResources: true,
+		execTraceConfig: new modelsv2.SimulateTraceConfig({
+			enable: true,
+			scratchChange: true,
+			stackChange: true,
+			stateChange: true,
+		}),
+		fixSigners: true,
+	});
+
+	const sim = ac.simulateTransactions(request);
+	sim.query.format = "json";
+
+	const result = await sim.doRaw();
+	const content = new TextDecoder("utf-8").decode(result);
+	return content;
+}
+
+async function decompile(client: LanguageClient, url: string, id: string): Promise<string | null> {
+	const ac = new algosdk.Algodv2("", url);
+	const app = await ac.getApplicationByID(BigInt(id)).do();
+
+	const response = await client.sendRequest("workspace/executeCommand", {
+		command: "teal.decompile",
+		arguments: {
+			bytecode: Buffer.from(app.params.approvalProgram).toString('base64'),
+		}
+	}) as { teal: string } | null;
+
+	if (!response) {
+		return null;
+	}
+
+	return response.teal;
+};
+
 export async function activate(context: vscode.ExtensionContext) {
 	const config = vscode.workspace.getConfiguration("tealsp");
+
+	const callbarProvider = new CallBarProvider(context);
+
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider('callbarview', callbarProvider));
 
 	await vscode.workspace.fs.createDirectory(context.globalStorageUri);
 
@@ -208,6 +418,85 @@ export async function activate(context: vscode.ExtensionContext) {
 		}
 
 		await tryUpdate(context, client);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand("teal.sourcemap.generate", async () => {
+		const content = await generateSourcemap(client);
+		if (!content) {
+			return;
+		}
+
+		const doc = await vscode.workspace.openTextDocument({
+			content,
+		});
+		await vscode.window.showTextDocument(doc);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand("teal.call.app", async () => {
+		const url = await pickNetworkAndReturnUrl();
+
+		if (url === undefined) {
+			return;
+		}
+
+		const id = await vscode.window.showInputBox({
+			prompt: "Enter the smart contract application ID to call",
+			placeHolder: "e.g. 12345678",
+			validateInput: (value) => {
+				const num = Number(value);
+				return !isNaN(num) && num >= 0 ? null : "Invalid application ID";
+			}
+		});
+
+		if (id === undefined) {
+			return;
+		}
+
+		const content = await call(url, id);
+		if (!content) {
+			vscode.window.showErrorMessage(`Failed to call application ID ${id}.`);
+			return;
+		}
+
+		const doc = await vscode.workspace.openTextDocument({
+			content
+		});
+
+		await vscode.window.showTextDocument(doc);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand("teal.decompile", async () => {
+		const url = await pickNetworkAndReturnUrl();
+
+		if (url === undefined) {
+			return;
+		}
+
+		const id = await vscode.window.showInputBox({
+			prompt: "Enter the TEAL program ID to decompile",
+			placeHolder: "e.g. 12345678",
+			validateInput: (value) => {
+				const num = Number(value);
+				return !isNaN(num) && num >= 0 ? null : "Invalid program ID";
+			}
+		});
+
+		if (id === undefined) {
+			return;
+		}
+
+		const content = await decompile(client, url, id);
+		if (!content) {
+			vscode.window.showErrorMessage(`Failed to decompile program ID ${id}.`);
+			return;
+		}
+
+		const doc = await vscode.workspace.openTextDocument({
+			content,
+			language: "teal"
+		});
+
+		await vscode.window.showTextDocument(doc);
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand("teal.goto.pc", async () => {
